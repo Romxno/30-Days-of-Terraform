c677ab52317562a63c03f2a09120b0b52442704b
Shubham Pauranik <144885013+Romxno@users.noreply.github.com>
2026-05-04 02:43:38 +0530

    Update main.tf

---
+++ day-03/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "main_vpc" {
+  cidr_block           = "10.0.0.0/16"
+
+  tags = {
+    Name = "day3-vpc"
+  }
+}
+
+# Create an S3 bucket with dependency
+resource "aws_s3_bucket" "day3_bucket" {
+  bucket = "shubham-day3-terraform"
+
+  tags = {
+    Name        = "shubham day3 bucket"
+    Environment = "Dev"
+    VPC         = aws_vpc.main_vpc.id
+  }
+}
